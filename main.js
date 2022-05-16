dc3f345591171b4770bcd43a172c3f29707aa761
Kazimir84 <kav2007@ukr.net>
2022-05-16 20:27:14 +0300

    Little fix.

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ let resHypotenuse = document.createElement('div');
 // console.log (5 < 6) -> true
 // console.log (5 == 6) -> false
 // console.log (5 > 6) -> false
-// console.log (5 ===! 6) -> false
+// console.log (5 === 6) -> false
 // console.log (10 === 10) -> true
 // console.log (10 >= 10) -> true
 // console.log (10 > 10)  -> false
